fd269da7132d3c76e9aaeb9afce9cdb5bf4ac946
Riguel Fabre <rfab@mobileu.localdomain>
2014-07-13 18:43:32 -0400

    add rdHTML

---
+++ rdRHTMLAssn.R
@@ -0,0 +1,6 @@
+#http://www.uefa.com/worldcup/season=2014/standings/index.html
+require(XML)
+
+theURL <- "http://www.uefa.com/worldcup/season=2014/standings/index.html"
+
+fifaStandings <- readHTML  
