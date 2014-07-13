27fd5095664b0cb878e19fc7b952e75668fb7be9
Riguel Fabre <rfab@mobileu.localdomain>
2014-07-13 18:47:14 -0400

    add rdHTML

--- rdRHTMLAssn.R
+++ rdRHTMLAssn.R
@@ -3,4 +3,4 @@ require(XML)
 
 theURL <- "http://www.uefa.com/worldcup/season=2014/standings/index.html"
 
-fifaStandings <- readHTML  
+fifaStandings <- readHTMLTable(theURL, which = 1, header = FALSE)     
