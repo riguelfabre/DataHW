a2ccdffc9e6021931513fcd3ec600e4c2849d7bd
riguelfabre <riguelfabre@users.noreply.github.com>
2014-07-13 18:45:32 -0400

    Delete rdRHTMLAssn.R

--- rdRHTMLAssn.R
+++
@@ -1,6 +0,0 @@
-#http://www.uefa.com/worldcup/season=2014/standings/index.html
-require(XML)
-
-theURL <- "http://www.uefa.com/worldcup/season=2014/standings/index.html"
-
-fifaStandings <- readHTML  
